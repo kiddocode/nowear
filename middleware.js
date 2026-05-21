7d9d08c1f1542a5d9f07b19fb4389e40071f00ac
Mayte <mnavarretegon@gmail.com>
2026-05-21 15:29:08 +0200

    fix middleware con @supabase/ssr

--- middleware.js
+++ middleware.js
@@ -1,9 +1,20 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll: () => req.cookies.getAll(),
+        setAll: (cookies) => cookies.forEach(({ name, value, options }) => res.cookies.set(name, value, options))
+      }
+    }
+  )
+
   const { data: { session } } = await supabase.auth.getSession()
 
   const isAuth = !!session
