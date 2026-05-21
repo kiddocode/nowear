68cfb57c75315e2faa6fcbee4aab02bf4f343353
Mayte <mnavarretegon@gmail.com>
2026-05-21 20:22:01 +0200

    redirect demo a demo/organizadora

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/demo',
+        destination: '/demo/organizadora',
+        permanent: false,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
